29077149ed000624e470af46f261b89ca15f226f
nyannkov <trickeyrabbit@yahoo.co.jp>
2020-05-27 19:23:54 +0900

    Fixed wrong variable type.

--- src/usbd/midi_cdc_core.c
+++ src/usbd/midi_cdc_core.c
@@ -83,7 +83,7 @@ static usb_cdc_send_status_t usb_cdc_send_status = USB_CDC_SEND_STATUS_INIT;
 static uint8_t current_buffer_page = 0;
 
 // current usb cdc acm data buffer position
-static uint8_t current_end_position = 0;
+static uint16_t current_end_position = 0;
 
 // usb cdc acm data send buffer (double buffer)
 static uint8_t usb_cdc_tx_buffer[USB_CDC_TX_BUF_PAGE_NUM][USB_CDC_TX_BUF_SIZE];
